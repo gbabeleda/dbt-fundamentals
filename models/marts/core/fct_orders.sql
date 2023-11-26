2e696695817222e24ff901cae46f99153e1e4d03
Gio Abeleda <gioabeleda@gmail.com>
2023-11-26 03:52:02 +0000

    First iteration of fct_orders

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,29 +1,30 @@
 with payments as (
-    select
-        orderid as order_id,
-        amount
-    
-    from {{ ref('stg_payments') }}
+    select * from {{ ref('stg_payments') }}
 ),
 
 orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+{# sum of amount per order#}
+order_payments as (
     select
         order_id,
-        customer_id
-    
-    from {{ ref('stg_orders') }}
+        sum(case when status = 'success' then amount end) as amount
+    from payments 
+
+    group by 1
 ),
 
 joined as (
-    select
+    select 
         order_id,
         customer_id,
-        amount
-    from payments 
+        order_date,
+        coalesce(order_payments.amount,0) as amount
 
-    join orders 
-
-    using(order_id)
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from joined
