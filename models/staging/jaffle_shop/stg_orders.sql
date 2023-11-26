32cbdf4df9f95671b115a644c9071ce5833fd0d9
Gio Abeleda <gioabeleda@gmail.com>
2023-11-26 04:28:52 +0000

    Added field for checking source freshness, stg_orders

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,7 +4,10 @@ with orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+
+        {# For checking source freshness #}
+        _etl_loaded_at
 
     from {{ source('jaffle_shop', 'orders') }}
 
